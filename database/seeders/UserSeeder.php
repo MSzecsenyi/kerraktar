6375d84a660fd7eb616fda60403122cfd83393af
Marton Szecsenyi <szecsenyi.marton@gmail.com>
2022-11-12 12:22:10 +0100

    added test users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,9 +18,9 @@ class UserSeeder extends Seeder
     {
         User::factory()->create([
             'name' => "admin",
-            'email' => "szecsenyi.marton@cserkesz.hu",
+            'email' => "admin",
             'email_verified_at' => now(),
-            'password' => Hash::make('password'), // password
+            'password' => 'pw', // password
             'remember_token' => Str::random(10),
             'district' => 5,
             'group_number' => 519,
@@ -29,6 +29,36 @@ class UserSeeder extends Seeder
             'is_storekeeper' => true,
             'is_admin' => true,
         ]);
+
+        User::factory()->create([
+            'name' => "raktaros",
+            'email' => "raktaros",
+            'email_verified_at' => now(),
+            'password' => 'pw', // password
+            'remember_token' => Str::random(10),
+            'district' => 5,
+            'group_number' => 519,
+            'phone' => +131323,
+            'is_group' => false,
+            'is_storekeeper' => true,
+            'is_admin' => false,
+        ]);
+
+        User::factory()->create([
+            'name' => "csapat",
+            'email' => "csapat",
+            'email_verified_at' => now(),
+            'password' => 'pw', // password
+            'remember_token' => Str::random(10),
+            'district' => 5,
+            'group_number' => 519,
+            'phone' => +131323,
+            'is_group' => true,
+            'is_storekeeper' => fasle,
+            'is_admin' => fasle,
+        ]);
+
+
 
         User::factory(10)->create();
     }
