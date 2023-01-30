c41ef490f592e1f0c40f3eb79906d3750518ddff
Marton Szecsenyi <szecsenyi.marton@gmail.com>
2023-01-30 22:07:34 +0100

    uuid

--- database/migrations/2022_12_28_171459_create_unique_item_table.php
+++ database/migrations/2022_12_28_171459_create_unique_item_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('unique_items', function (Blueprint $table) {
-            $table->uuid('id')->primary()->default(DB::raw('uuid_generate_v4()'));
+            $table->uuid('id')->primary()->default(DB::raw('public.uuid_generate_v4()'));
             $table->timestamps();
             $table->foreignId('item_id')->constrained();
             $table->string('alt_name')->nullable();
