ad9fd01690ac1b580e4d17ff4f82c8cb85249d04
Marton Szecsenyi <szecsenyi.marton@gmail.com>
2023-04-18 21:23:08 +0200

    force https maybe

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\DB;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +28,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::defaultView('vendor/pagination/paginator');
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
         // INFO: kiírja db comm közben, hogy éppen milyen queryket használ
         // DB::listen(function ($query) {
         //     dump([$query->sql, $query->bindings]);
