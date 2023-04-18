26ce298ef43792bb29c9f9681403cf3ad467d8af
Marton Szecsenyi <szecsenyi.marton@gmail.com>
2023-04-18 23:38:39 +0200

    force password creation

---
+++ app/Http/Middleware/IsPasswordSetMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsPasswordSetMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        error_log()
+        if (auth()->check() && auth()->user()->password == 'not_created' && !$request->password && !$request->is('setpassword')) {
+            return redirect()->route('setpassword');
+        }
+        return $next($request);
+    }
+}
