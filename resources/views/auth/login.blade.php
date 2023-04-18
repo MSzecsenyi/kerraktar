365856079f69d8c57e6319dddd25537ed688320b
Marton Szecsenyi <szecsenyi.marton@gmail.com>
2023-04-18 19:54:37 +0200

    app js to check vite

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <x-application-logo class="w-20 h-20 text-gray-500 fill-current" />
             </a>
         </x-slot>
 
@@ -19,7 +19,7 @@
             <div>
                 <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                <x-input id="email" class="block w-full mt-1" type="email" name="email" :value="old('email')" required
                     autofocus />
             </div>
 
@@ -27,7 +27,7 @@
             <div class="mt-4">
                 <x-label for="password" :value="__('Jelszó')" />
 
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                <x-input id="password" class="block w-full mt-1" type="password" name="password" required
                     autocomplete="current-password" />
             </div>
 
@@ -35,7 +35,7 @@
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox"
-                        class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                        class="text-indigo-600 border-gray-300 rounded shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                         name="remember">
                     <span class="ml-2 text-sm text-gray-600">{{ __('Maradjak bejelentkezve') }}</span>
                 </label>
@@ -43,7 +43,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900"
+                    <a class="text-sm text-gray-600 underline hover:text-gray-900"
                         href="{{ route('password.request') }}">
                         {{ __('Elfelejtetted a jelszavad?') }}
                     </a>
