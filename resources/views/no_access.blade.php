7f4064ad05c2f627c455fa499b3386160930f621
MSzecsenyi <szecsenyi.marton@gmail.com>
2023-04-26 16:22:34 +0200

    deleted cartegory name, changed google play icon

--- resources/views/no_access.blade.php
+++ resources/views/no_access.blade.php
@@ -12,11 +12,9 @@
                     </p>
                     <div class="mt-4">
                         <div class="flex items-center justify-center">
-                            <a href="https://play.google.com/store/apps/details?id=com.example.myapp" target="_blank"
-                                class="block">
-                                <img class="w-56 mx-auto" src="{{ asset('src/google-play-badge.png') }}"
-                                    alt="Google Play">
-                            </a>
+                            
+                                <a href='https://play.google.com/store/apps/details?id=com.google.android.googlequicksearchbox&hl=en&gl=US&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'  class="w-52"/></a>
+
                         </div>
                         <div class="flex items-center justify-center">
                             <a href="https://apps.apple.com/us/app/google/id284815942?itsct=apps_box_badge&amp;itscg=30200"
